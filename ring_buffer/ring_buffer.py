0e44d15affec07db68e5a00ca93bd08c4b134819
LuisFelipeUrena <60860402+LuisFelipeUrena@users.noreply.github.com>
2020-07-17 16:14:46 -0400

    updated buffer

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -2,20 +2,32 @@ from collections import deque
 class RingBuffer:
     def __init__(self, capacity):
         self.capacity = capacity
-        self.ls = []
+        self.ls = deque(maxlen=self.capacity)
 
         
 
     def append(self, item):
+        # self.ls.append(item)
         if len(self.ls) == self.capacity:
-            self.ls.pop(0)
-        # if len(self.ls) is self.capacity+1:
-        #     self.ls.insert(0,item)    
-        self.ls.append(item) 
+            self.ls.appendleft(item)
+        else:
+            self.ls.append(item)    
+        
+        
+        
+        
+
+
+        # # self.ls.insert(0,item)
+        # if len(self.ls) == self.capacity:
+        #     self.ls.pop(0)
+        # # # if len(self.ls) is self.capacity+1:
+        # # #     self.ls.insert(0,item)    
+        # self.ls.append(item) 
          
     
 
     def get(self):
-        return self.ls
+        return list(self.ls)
         
         
