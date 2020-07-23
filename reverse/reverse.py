be7640e39b9f8df0371f732cdfb4a5592af55ee8
LuisFelipeUrena <60860402+LuisFelipeUrena@users.noreply.github.com>
2020-07-23 17:40:26 -0400

    new commit

--- reverse/reverse.py
+++ reverse/reverse.py
@@ -1,3 +1,4 @@
+import sys
 class Node:
     def __init__(self, value=None, next_node=None):
         self.value = value
@@ -41,3 +42,27 @@ class LinkedList:
     def reverse_list(self, node):
         # print the reverse value of the list
         # if the node has a next pointercd
+        # if node is None or node.next_node is None:
+        #     return node
+        # rest = self.reverse_list(node.next_node)
+        # self.head.next_node.next_node = node
+        # self.head.set_next(None)
+        # return rest
+        # sys.setrecursionlimit(1500)
+      
+
+        if self.head is None:
+            return None
+        current  = self.head
+        next_ = current.next_node
+        if next_ is None:
+            self.reverse_list(next_)    
+        prev = None
+        current = self.head
+        while (current is not None):
+                
+            next_ = current.next_node
+            current.next_node = prev
+            prev = current
+            current = next_
+        self.head = prev    
--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,19 +1,47 @@
+# crete a list with a maximum capacity that's predifined as a parameter in the 
+# class.
+# if the list is empty or not at full capacity fill the list as a stack
+# if the list is full replace each number in the list according to the index
+# [1,2,3] is a full stack
+# when adding 4 we should replace 1 with 4
+# then 
+
 from collections import deque
 class RingBuffer:
     def __init__(self, capacity):
         self.capacity = capacity
-        self.ls = deque(maxlen=self.capacity)
+        self.ls = deque([],maxlen=self.capacity)
 
         
 
     def append(self, item):
-        self.ls.insert(0,item)
+        replacement = 0
+        if len(self.ls) < self.capacity:
+            return self.ls.append(item)
         if len(self.ls) == self.capacity:
-            self.ls.popleft()
+            self.ls.remove(self.ls[replacement])
+            self.ls.insert(replacement,item)
+            replacement +=1
+        if replacement == self.capacity:
+            replacement = 0
+
+
+
+
+ 
+
+
+
+             
+        # else:
+        #     self.ls.appendleft()        
+        # self.ls.insert(0,item) 
+        # if len(self.ls) == self.capacity:
+        #     self.ls.popleft()
             
             
         
-        return self.ls    
+        # return self.ls    
          
          
          
